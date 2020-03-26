dfd159590f8037b361404361ca71ccfc6fd118aa
chadcarlson <chad.carlson@platform.sh>
2020-03-26 14:38:09 -0400

    Update text.

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -1,6 +1,6 @@
 const config = require("platformsh-config").config();
 
-var backend_route = config.getRoute("strapi").url;
+// var backend_route = config.getRoute("strapi").url;
 
 require("dotenv").config({
   path: `.env.${process.env.NODE_ENV}`,
@@ -24,7 +24,8 @@ module.exports = {
     {
       resolve: "gatsby-source-strapi",
       options: {
-        apiURL: backend_route.substring(0, backend_route.length - 1),
+        // apiURL: backend_route.substring(0, backend_route.length - 1),
+        apiURL: "https://www.backend.pr-1-djjnuwy-muwzogvpcpoe2.eu-3.platformsh.site",
         contentTypes: [
           // List of the Content Types you want to be able to request from Gatsby.
           "article",
