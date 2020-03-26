f248a46acdc9a1963059138c43af8059d261d86c
chadcarlson <chad.carlson@platform.sh>
2020-03-26 14:12:20 -0400

    Raw string + test.

---
+++ frontend/test.js
@@ -0,0 +1,5 @@
+const config = require("platformsh-config").config();
+
+console.log("https://www.backend.pr-1-djjnuwy-muwzogvpcpoe2.eu-3.platformsh.site")
+
+console.log(config.getRoute("strapi").url.substring(0, str.length - 1))
