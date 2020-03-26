a4794583512fefe46b38277851e8381025754fc7
chadcarlson <chad.carlson@platform.sh>
2020-03-26 14:18:56 -0400

    Update test.

--- frontend/test.js
+++ frontend/test.js
@@ -2,4 +2,6 @@ const config = require("platformsh-config").config();
 
 console.log("https://www.backend.pr-1-djjnuwy-muwzogvpcpoe2.eu-3.platformsh.site")
 
-console.log(config.getRoute("strapi").url.substring(0, str.length - 1))
+var backend_route = config.getRoute("strapi").url
+console.log(backend_route)
+console.log(backend_route.substring(0, backend_route.length - 1))
